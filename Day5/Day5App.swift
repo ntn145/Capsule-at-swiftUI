81fbc67015954321890cdaedf06cb2f78a0b9478
Nhan Nguyen Trong <89365881+ntn145@users.noreply.github.com>
2022-08-09 21:24:59 +0700

    Initial Commit

---
+++ Day5/Day5App.swift
@@ -0,0 +1,17 @@
+//
+//  Day5App.swift
+//  Day5
+//
+//  Created by Khoai Alpha on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
